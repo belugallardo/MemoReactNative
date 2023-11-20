43abbd4eece8b6a6a9f7cae95965ec40fb4440b8
Belu Gallardo <belugallardo@Mac-mini-de-Emiliano.local>
2023-11-20 17:28:22 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Esta es la primera App de Memo androide  🙄 !</Text>
+      <Text> esto es un poco basico me aburro</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
