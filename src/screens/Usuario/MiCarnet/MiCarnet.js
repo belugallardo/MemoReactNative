61de01d9c9ebc95808586baac73ca66ed3309cfe
emybr <emybr82ar@gmail.com>
2024-02-13 02:07:12 -0300

    carnet

--- src/screens/Usuario/MiCarnet/MiCarnet.js
+++ src/screens/Usuario/MiCarnet/MiCarnet.js
@@ -1,12 +1,110 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity,Image } from 'react-native'
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useGetUserQuery } from '../../../fectures/api/apiSlice';
+import { useFocusEffect } from '@react-navigation/native';
 
-const MiCarnet = () => {
-  return (
-    <View>
-      <Text>MiCarnet</Text>
-    </View>
-  )
+const MiCarnet = ({navigation}) => {
+  const authState = useSelector((state) => state.auth);
+  const email = authState.value.email;
+  const { data, isLoading, error, refetch } = useGetUserQuery(email);
+  const apodo = data?.data.card[0];
+  const info = data?.data.card[1];
+  const contacto = data?.data.card[2];
+  const goHomeUsuario = () => {
+    navigation.navigate('HomeUsuario');
 }
+
+useFocusEffect(
+  React.useCallback(() => {
+      refetch();
+  }, [])
+);
+  return (
+    <View style={styles.containerCarnet} >
+    <Text style={styles.textTitle}>HOLA!</Text>
+    <View style={styles.contenedorInput}>
+    <Text style={styles.textCarnet}>MI APODO ES:</Text>
+
+        <Text style={styles.inputRegistro}>{apodo}</Text>
+        
+        <Text style={styles.textCarnet}>PARA ENTENDERTE MEJOR NECESITO:</Text>
+        <Text style={styles.inputRegistro2}>{info} </Text>
+
+        <Text style={styles.textCarnet}>CONTACTO DE EMERGENCIA:</Text>
+        <Text style={styles.inputRegistro}>{contacto} </Text>
+    </View>
+    <View style={styles.contenedorBotonAzul}>
+        <TouchableOpacity onPress={goHomeUsuario} >
+            <Image source={require('../../../../assets/back.png')} style={styles.botonAzul} />
+        </TouchableOpacity>
+    </View>
+</View>
+)
+}
+const styles = StyleSheet.create({
+containerCarnet: {
+flex: 1,
+backgroundColor: '#eec144',
+padding:15
+},
+textTitle: {
+color: '#ff6600',
+fontSize: 45,
+fontWeight: '900',
+marginLeft: 30,
+marginTop: 70,
+},
+textCarnet: {
+color: '#ffffff',
+fontSize: 23,
+fontWeight: '600',
+marginTop: 20,
+},
+inputRegistro: {
+width: 300,
+marginTop: 30,
+marginBottom: 20,
+padding: 10,
+borderRadius: 20,
+backgroundColor: '#ffffff',
+color: '#737474',
+fontSize: 16,
+width: '88%',
+fontWeight: 'bold',
+},
+inputRegistro2: {
+width: 300,
+marginTop: 10,
+marginBottom: 10,
+padding: 10,
+borderRadius: 20,
+backgroundColor: '#ffffff',
+color: '#737474',
+fontSize: 16,
+width: '88%',
+height: 100,
+fontWeight: 'bold',
+},
+contenedorInput:{
+marginLeft:20,
+},
+contenedorBotonAzul: {
+flexDirection: 'row',
+justifyContent: 'space-between',
+width: '80%',
+marginLeft: 30,
+marginTop: 60,
+justifyContent: 'center',
+alignItems: 'center',
+},
+botonAzul: {
+width: 60,
+height: 60,
+borderRadius: 100,
+backgroundColor: '#0e3ed9',
+
+},
+});
 
 export default MiCarnet
--- src/screens/Usuario/RutinaUsuario/RutinaUsuario.js
+++ src/screens/Usuario/RutinaUsuario/RutinaUsuario.js
@@ -1,119 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { View, Text, StyleSheet, FlatList, Image , TouchableOpacity} from 'react-native';
-// import { useSelector } from 'react-redux';
-// import { useGetRutinaEmailQuery } from '../../../fectures/api/apiSlice';
-// import homeUsuario from '../../../../assets/home.png';
-
-// const RutinaUsuario = ({navigation}) => {
-//   const [dayOfWeek, setDayOfWeek] = useState('');
-//   const [datosAlmacenados, setDatosAlmacenados] = useState({});
-//   const authState = useSelector((state) => state.auth);
-//   const email = authState.value.email;
-//   const { data } = useGetRutinaEmailQuery(email);
-
-//   const goHomeUsuario = () => {
-//     navigation.navigate('HomeUsuario')
-// }
-
-//   const getDayOfWeek = () => {
-//     const days = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
-//     const dayIndex = new Date().getDay();
-//     setDayOfWeek(days[dayIndex]);
-//   };
-
-//   useEffect(() => {
-//     getDayOfWeek();
-//   }, []);
-
-//   useEffect(() => {
-//     if (data && data.document) {
-//       const rutinaDia = data.document[0].rutina[dayOfWeek]; // Obtenemos las rutinas solo para el día actual
-//       setDatosAlmacenados(rutinaDia);
-//     }
-//   }, [dayOfWeek, data]);
-
-//   return (
-//     <View>
-//       <Text style={styles.title}>{dayOfWeek}</Text>
-//       <FlatList
-//         data={Object.keys(datosAlmacenados)}
-//         keyExtractor={(item, index) => index.toString()}
-//         renderItem={({ item }) => (
-//           <View>
-//             <Text style={styles.subTitle}>{item}</Text>
-//             <FlatList
-//               data={datosAlmacenados[item]}
-//               keyExtractor={(subItem, subIndex) => subIndex.toString()}
-//               renderItem={({ item: subItem }) => (
-//                 <Image source={{ uri: subItem }} style={styles.imageStyle} />
-//               )}
-//               style={styles.flatList}
-//             />
-//           </View>
-//         )}
-//       />
-//                   <View style={styles.blueButtonContainer}>
-//                 <TouchableOpacity style={styles.blueButton} onPress={goHomeUsuario}>
-//                     <Image source={homeUsuario} style={styles.imageStyleButton} />
-//                 </TouchableOpacity>
-//                 <TouchableOpacity style={styles.blueButton} onPress={goHomeUsuario}>
-//                     <Image source={homeUsuario} style={styles.imageStyleButton} />
-//                 </TouchableOpacity>
-//             </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   title: {
-//     fontSize: 40,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//     margin: 30,
-//   },
-//   subTitle: {
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//     marginVertical: 10,
-//   },
-//   imageStyle: {
-//     width: 90,
-//     height: 90,
-//     margin: 5,
-//   },
-//   subTitle:{
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//     marginVertical: 10,
-//   },
-//   flatList:{
-//     display: 'flex',
-//     flexDirection: 'row',
-//   },
-//   blueButton: {
-//     width: 30,
-//     height: 60,
-//     marginTop: 1,
-//     borderRadius: 100,
-//     backgroundColor: '#0a06f1',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-// },
-// blueButtonContainer: {
-//   flexDirection: 'row',
-//   justifyContent: 'space-between',
-//   margin: 35,
-//   marginHorizontal:45,
-// },
-// imageStyleButton: {
-//   width: 70,
-//   height: 70,
-// },
-// });
-
-// export default RutinaUsuario;
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, Image , TouchableOpacity} from 'react-native';
 import { useSelector } from 'react-redux';
@@ -143,27 +27,30 @@ const RutinaUsuario = ({navigation}) => {
 
   useEffect(() => {
     if (data && data.document) {
-      const rutinaDia = data.document[0].rutina[dayOfWeek]; // Obtenemos las rutinas solo para el día actual
+      const rutinaDia = data.document[0].rutina[dayOfWeek]; 
       setDatosAlmacenados(rutinaDia);
     }
   }, [dayOfWeek, data]);
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
-      refetch(); // Realizar refetch al enfocar en esta pantalla
+      refetch(); 
     });
 
     return unsubscribe;
   }, [navigation, refetch]);
 
+
+  const diaEnMayuscula = dayOfWeek.toUpperCase();
+  
   return (
-    <View>
-      <Text style={styles.title}>{dayOfWeek}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{diaEnMayuscula}</Text>
       <FlatList
         data={datosAlmacenados ? Object.keys(datosAlmacenados) : []}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
-          <View>
+          <View style={styles.containerSubtitle}>
             <Text style={styles.subTitle}>{item}</Text>
             <FlatList
               data={datosAlmacenados[item]}
@@ -172,6 +59,7 @@ const RutinaUsuario = ({navigation}) => {
                 <Image source={{ uri: subItem }} style={styles.imageStyle} />
               )}
               style={styles.flatList}
+              horizontal={true}
             />
           </View>
         )}
@@ -187,28 +75,32 @@ const RutinaUsuario = ({navigation}) => {
 };
 
 const styles = StyleSheet.create({
+  container:{
+    marginTop: 50,
+  },
   title: {
     fontSize: 40,
     fontWeight: 'bold',
     textAlign: 'center',
     margin: 30,
+    color: '#ff6600'
   },
   subTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: '700',
     textAlign: 'center',
     marginVertical: 10,
+    textTransform:'uppercase',
+    color: '#2372d9',
+  },
+  containerSubtitle:{
+alignItems:'flex-start',
+marginLeft: 20,
   },
   imageStyle: {
     width: 90,
     height: 90,
     margin: 5,
-  },
-  subTitle:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginVertical: 10,
   },
   flatList:{
     display: 'flex',
@@ -233,6 +125,9 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
   },
+  flatList:{
+    margin: 10,
+  }
 });
 
 export default RutinaUsuario;
